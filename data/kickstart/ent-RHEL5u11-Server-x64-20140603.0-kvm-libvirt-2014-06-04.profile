93f7a6daaf7d389550de9b2789b5ab75f8d50756
sgao <sgao@redhat.com>
2014-06-17 18:50:19 +0800

    generate virt-who kickstart file

---
+++ data/kickstart/ent-RHEL5u11-Server-x64-20140603.0-kvm-libvirt-2014-06-04.profile
@@ -0,0 +1,4 @@
+[General]
+distro = RHEL5u11-Server-x86_64-20140603.0
+kickstart = libvirt/RHEL5/ent-ks-RHEL5u11-Server-x64-20140603.0-kvm-libvirt-2014-06-04.cfg
+
--- utils/shell/virt-who-kickstart.sh
+++ utils/shell/virt-who-kickstart.sh
@@ -4,29 +4,57 @@
 
 source comment-line.sh
 
+function check_builds(){
+
+}
+
+function check_distros(){
+RHEL-7u0-Server-x64-20140507.0.distro
+}
+
+function create_profiles(){
+
+}
+
+function create_kickstarts(){
+
+}
 
 
+if [ check_builds ]; then
+    star_line "check git repo"
+    if ! [ -d repo ]; then
+        star_line "git repo not exist, clone now"
+        git clone git+ssh://git@qe-git.englab.nay.redhat.com/~/repo/virt-qe/repo
+    fi
 
+    star_line "run git pull"
+    git pull
 
+    star_line "run git status"
+    git status
 
-star_line "check git repo"
+    while ! [ check_distros ]
+    do
+        if [ check_distros ]; then
+            create_profiles
+            create_kickstarts
 
-if ! [ -d repo ]; then
-    star_line "git repo not exist, cloning now"
-    #git clone git+ssh://git@qe-git.englab.nay.redhat.com/~/repo/virt-qe/repo
+            star_line "run git add"
+            git add *** ***
+
+            star_line "run git commit"
+            git commit -m "sdfsadf"
+
+            star_line "run git push"
+            git push
+
+            exit $?
+        else
+            star_line "distros file not exist yet, continue ..."
+        fi
+    done
+
+else
+    star_line "no new build available now, quite ... "
 fi
-
-git pull
-
-git status
-
-create_profiles
-create_kickstarts
-
-git add *** ***
-
-git commit -m "sdfsadf"
-
-git push
-
-exit $?
