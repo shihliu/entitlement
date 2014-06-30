44d9ca9ac28527e4a101caf3c170a3199a72a0f4
sgao <sgao@redhat.com>
2014-06-30 11:26:20 +0800

    change local run method with time out

--- utils/tools/shell/localsh.py
+++ utils/tools/shell/localsh.py
@@ -23,7 +23,6 @@ class LocalSH(object):
     def run_subprocess(self, cmd, timeout=None):
         """
         Executes SSH command on local machine.
-        if running cmd with large bulk of output, it's easy to be blocked
         """
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
         # if timeout is not set wait for process to complete
@@ -34,18 +33,15 @@ class LocalSH(object):
             terminate_time = time.time() + timeout
             while process.poll() == None:
                 logger.debug("Command running, wait 1 minute ...")
-                time.sleep(60)
+                time.sleep(1)
                 if terminate_time < time.time():
-                    # starting 2.6 subprocess has a kill() method which is preferable
                     logger.debug("Kill process, return -1 ...")
+                    process.kill()
                     retcode = -1
                     stdout = "Command terminated due to timeout ..."
-                    process.kill()
+                    return retcode, stdout
             retcode = process.wait()
             stdout, stderr = process.communicate()
-#                 os.kill(process.pid, signal.SIGKILL)
-#                 raise OSError("Process timeout has been reached")
-#             retcode = process.returncode
         return retcode, stdout
 
     @classmethod
@@ -59,7 +55,6 @@ class LocalSH(object):
         os.system(cmd)
     @classmethod
     def run_commands(self, cmd, timeout=None):
-        '''not blocking method'''
         import commands
         retcode, stdout = commands.getstatusoutput(cmd)
         return retcode, stdout
@@ -70,4 +65,4 @@ class LocalSH(object):
 
 if __name__ == "__main__":
 #     LocalSH.local_run("/usr/bin/virt-install --network=bridge:br0 --initrd-inject=/root/workspace/entitlement/data/kickstart/unattended/rhel-server-6-series.ks --extra-args \"ks=file:/rhel-server-6-series.ks\" --name=AUTO-SAM-1.4.0-RHEL-6-20140512.0 --disk path=/home/auto-imgs/AUTO-SAM-1.4.0-RHEL-6-20140512.0.img,size=20 --ram 2048 --vcpus=2 --check-cpu --accelerate --hvm --location=http://download.englab.nay.redhat.com/pub/rhel/released/RHEL-6/6.5/Server/x86_64/os/ ", 10)
-    LocalSH.local_run("ifconfig", 20)
+    LocalSH.local_run("sleep 10", 5)
