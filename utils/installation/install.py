d41982689963d69bafb69b3da5c9fce37ed8d5a2
sgao <sgao@redhat.com>
2014-07-10 17:25:20 +0800

    rhel 5 builds have not STATUS file, juest skip check it now

--- utils/installation/install.py
+++ utils/installation/install.py
@@ -45,16 +45,19 @@ class Install(object):
             return "No New Build"
 
     def check_build_status(self, build):
-        # check STATUS file, if FINISHED, begin installing
-        status_file = constants.get_build_tree(build) + "/" + build + "/" + "STATUS"
-        logger.info("Status file is : %s." % status_file)
-        while True:
-            if "FINISHED" in htmlsource.get_html_source(status_file):
-                logger.info("Build %s is in Finished status, going on ..." % build)
-                break
-            else:
-                time.sleep(60)
-                logger.info("Build %s is not in Finished status yet, wait 1 minute ..." % build)
+        if "RHEL5" in build:
+            pass
+        else:
+            # check STATUS file, if FINISHED, begin installing
+            status_file = constants.get_build_tree(build) + "/" + build + "/" + "STATUS"
+            logger.info("Status file is : %s." % status_file)
+            while True:
+                if "FINISHED" in htmlsource.get_html_source(status_file):
+                    logger.info("Build %s is in Finished status, going on ..." % build)
+                    break
+                else:
+                    time.sleep(60)
+                    logger.info("Build %s is not in Finished status yet, wait 1 minute ..." % build)
 
     def install_host(self):
         raise NotImplementedError, "Cannot call abstract method"
