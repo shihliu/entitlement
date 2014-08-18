7f9b298a48fa814f792311b1225073d2dfc62cc0
sgao <sgao@redhat.com>
2014-08-18 14:48:36 +0800

    change result check

--- data/beakertasks/entitlement-qa/Regression/virt-who/runtest.sh
+++ data/beakertasks/entitlement-qa/Regression/virt-who/runtest.sh
@@ -82,7 +82,7 @@ EOF"
             cases_params_list=$(python libvirt-test-beaker-api.py --handleguest=$HANDLEGUEST --samhostname=$SAMHOSTNAME --confile=$CONFILE --copyimages=$COPYIMAGES --samhostip=$SAMHOSTIP --targetmachine_ip=$CLIENTS --targetmachine_hostname=$CLIENTS --beaker=yes 2>&1 >/dev/null)
             for i in $cases_params_list; do
                 result=$(python libvirt-test-beaker-api.py $i)
-                if [ $result ]; then
+                if [ $result -eq 0 ]; then
                     rhts-report-result $i PASS result/default/$i
                 else
                     rhts-report-result $i FAIL result/default/$i
