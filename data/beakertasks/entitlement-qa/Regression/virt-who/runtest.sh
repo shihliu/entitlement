1cf9dde1286292e602dd399c0097f98a8bc4d80c
gaoshang <gaoshang@dhcp-65-198.nay.redhat.com>
2014-08-19 14:14:17 +0800

    modify virt-who task

--- data/beakertasks/entitlement-qa/Regression/virt-who/runtest.sh
+++ data/beakertasks/entitlement-qa/Regression/virt-who/runtest.sh
@@ -81,8 +81,8 @@ EOF"
             rlRun "echo \"Servers: $SERVERS\""
             cases_params_list=$(python libvirt-test-beaker-api.py --handleguest=$HANDLEGUEST --samhostname=$SAMHOSTNAME --confile=$CONFILE --copyimages=$COPYIMAGES --samhostip=$SAMHOSTIP --targetmachine_ip=$CLIENTS --targetmachine_hostname=$CLIENTS --beaker=yes 2>&1 >/dev/null)
             for i in $cases_params_list; do
-                result=$(python libvirt-test-beaker-api.py $i)
-                if [ $result -eq 0 ]; then
+                python libvirt-test-beaker-api.py $i
+                if [ $? -eq 0 ]; then
                     rhts-report-result $i PASS result/default/$i
                 else
                     rhts-report-result $i FAIL result/default/$i
