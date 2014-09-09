5e60b02e6cd01323ae2799e16b0e52103580104d
sgao <sgao@redhat.com>
2014-09-09 09:51:17 +0800

    run test cases in folder

--- testcases/test_rhsm_gui.py
+++ testcases/test_rhsm_gui.py
@@ -10,31 +10,19 @@ class Test_RHSM_GUI(unittest.TestCase):
         pass
 
     def testAll(self):
-        cases_list = ["tc_ID000000_GUI_list_all_objects", "tc_ID115150_GUI_register", "tc_ID115127_GUI_display_user_org", "tc_ID115139_GUI_list_available_pools"]
-        from testcases.rhsmgui.tc_ID000000_GUI_list_all_objects import tc_ID000000_GUI_list_all_objects
-        from testcases.rhsmgui.tc_ID115150_GUI_register import tc_ID115150_GUI_register
-        from testcases.rhsmgui.tc_ID115127_GUI_display_user_org import tc_ID115127_GUI_display_user_org
-        from testcases.rhsmgui.tc_ID115139_GUI_list_available_pools import tc_ID115139_GUI_list_available_pools
-        self.assertEqual(tc_ID000000_GUI_list_all_objects().run(), 0, "tc_ID000000_GUI_list_all_objects")
-        self.assertEqual(tc_ID115150_GUI_register().run(), 0, "tc_ID115150_GUI_register")
-        self.assertEqual(tc_ID115127_GUI_display_user_org().run(), 0, "tc_ID115127_GUI_display_user_org")
-        self.assertEqual(tc_ID115139_GUI_list_available_pools().run(), 0, "tc_ID115139_GUI_list_available_pools")
+        self.regressionTest()
 
     def regressionTest(self):
-#         path = os.path.abspath(os.path.realpath(os.path.join(os.path.dirname(__file__), os.pardir, "testcases/rhsmgui/")))
-#         files = os.listdir(path)
-#         case = re.compile("tc_ID0000", re.IGNORECASE)
-#         files = filter(case.search, files)
-#         filenameToModuleName = lambda f: os.path.splitext(f)[0]
-#         moduleNames = map(filenameToModuleName, files)
-#         sys.path.append(path)
-#         for testcase in moduleNames:
-#             obj = __import__(testcase)
-#             print dir(obj)
-#             getattr(getattr(obj, testcase)(), "run")(self)
-        pass
-
-
+        path = os.path.abspath(os.path.realpath(os.path.join(os.path.dirname(__file__), os.pardir, "testcases/rhsmgui/")))
+        files = os.listdir(path)
+        case = re.compile("tc_ID", re.IGNORECASE)
+        files = filter(case.search, files)
+        filenameToModuleName = lambda f: os.path.splitext(f)[0]
+        moduleNames = map(filenameToModuleName, files)
+        sys.path.append(path)
+        for testcase in moduleNames:
+            obj = __import__(testcase)
+            getattr(getattr(obj, testcase)(), "run")()
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
